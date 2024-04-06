7bbb7e059f4d9d7dda0dfd80d2cf1bcdc4e302f3
mombe <h220462g@hit.ac.zw>
2024-04-06 21:50:48 +0200

    Initial Save For Integration

---
+++ src/test/java/org/thefinal/lecturerscompanionv2/LecturersCompanionV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thefinal.lecturerscompanionv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecturersCompanionV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
